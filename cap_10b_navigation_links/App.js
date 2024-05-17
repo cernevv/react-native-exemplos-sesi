d13233e1ac76200df208672b94e20ef8db011751
Laura Cernev <laura.cernev24@gmail.com>
2024-05-17 15:13:05 -0300

    up

--- cap_10b_navigation_links/App.js
+++ cap_10b_navigation_links/App.js
@@ -1,18 +1,47 @@
-import React from 'react';
-import Evento from './components/Evento';
-import UsuarioGithub from './components/UsuarioGithub';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { NavigationContainer } from '@react-navigation/native';
+import React from "react";
+import { Button, View, StyleSheet } from "react-native";
+import Evento from "./components/Evento";
+import UsuarioGithub from "./components/UsuarioGithub";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+function TelaInicial({ navigation }) {
+  return (
+    <View style={estilos.container}>
 
-const Drawer = createDrawerNavigator();
+      <View style={estilos.botaocontainer}>
+        <Button title="Evento" onPress={() => navigation.navigate("Evento")} />
+     </View>
 
+ 
+     <View style={estilos.botaocontainer}>
+      <Button
+        style={estilos.botao}
+        title="Usuário GitHub"
+        onPress={() => navigation.navigate("Github")}
+      />
+    </View>
+    </View>
+  );
+}
+const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator>
-        <Drawer.Screen name="Evento" component={Evento} />
-        <Drawer.Screen name="Github" component={UsuarioGithub} />
-      </Drawer.Navigator>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={TelaInicial} />
+        <Stack.Screen name="Evento" component={Evento} />
+        <Stack.Screen name="Github" component={UsuarioGithub} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
-  }
+}
+const estilos = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  botaocontainer: {
+    marginVertical: 10,
+  },
+});
